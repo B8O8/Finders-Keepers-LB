f364b6548c84c8c70ed568c31827cedeb7a99d65
B8O8 <charbel_makhlouf76@live.com>
2026-07-03 18:50:21 +0300

    Import ActivityLogsModule into PosImportModule (fix DI)

--- finders-keepers-api/src/modules/pos-import/pos-import.module.ts
+++ finders-keepers-api/src/modules/pos-import/pos-import.module.ts
@@ -3,11 +3,13 @@ import { JwtModule } from '@nestjs/jwt';
 import { MulterModule } from '@nestjs/platform-express';
 import { PosImportController } from './pos-import.controller';
 import { PosImportService } from './pos-import.service';
+import { ActivityLogsModule } from '../activity-logs/activity-logs.module';
 
 @Module({
   imports: [
     JwtModule.register({}),
     MulterModule.register({ limits: { fileSize: 10 * 1024 * 1024 } }), // 10MB
+    ActivityLogsModule,
   ],
   controllers: [PosImportController],
   providers: [PosImportService],
